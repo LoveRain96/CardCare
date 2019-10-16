a9218e4edbc341704fb4af721577627b4bd01bcf
Duc <ducdt@sphinx-software.com>
2019-10-16 17:13:54 +0700

    add list component and remove cardIO

--- src/screens/main-view/HomeScreen.js
+++ src/screens/main-view/HomeScreen.js
@@ -1,12 +1,18 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import HeaderLeft from '../../components/HeaderLeft';
 import InputSearch from '../../components/InputSearch';
+import {ButtonGroup} from 'react-native-elements';
 
 const HomeScreen = props => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const onOpenModal = useCallback(() => {
     props.navigation.navigate('Search');
   }, [props]);
+  const updateIndex = useCallback(index => {
+    console.log(index);
+    setSelectedIndex(index);
+  }, []);
 
   useEffect(() => {
     props.navigation.setParams({
@@ -17,7 +23,18 @@ const HomeScreen = props => {
 
   return (
     <View style={styles.container}>
-      <Text>List</Text>
+      <View style={{paddingTop: 5}}>
+        <ButtonGroup
+          onPress={updateIndex}
+          selectedIndex={selectedIndex}
+          buttons={['Recommended', 'All', 'Updated']}
+          containerStyle={{
+            height: 30,
+            borderRadius: 25,
+            borderColor: '#2089dc',
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -33,8 +50,7 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
 });
